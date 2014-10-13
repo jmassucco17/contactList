d42a338f58f859c5e3d3f3520951687c811ea14a
Christian Lozano <lozanoc@wit.edu>
2014-10-13 16:23:57 -0400

    Update contactList.h
    pointers, constructor, deconstructors

--- contactList.h
+++ contactList.h
@@ -1,14 +1,51 @@
-//
-//  contactList.h
-//  
-//
-//  Created by James Massucco on 9/24/14.
-//
-//
-
-
-#include <iostream>
-#include <time.h>
-#include <fstream>
+#ifndef contactInfo_H
+#define contactInfo_H
+using namespace std;
 #include <string>
-#include <stdlib.h> //for atoi (string to integer)
+// Code written by Christian Lozano
+
+class contactInfo{
+public:
+	string getName() { return name; }
+	string getContact() { return contact; }
+	string getResidence() { return residence; }
+	string getWnum() { return wnum; }
+	string getNumb() { return numb; }
+	contactInfo::contactInfo(){
+		name = "none";
+		contact = "none";
+		residence = "none;";
+		wnum = "none";
+		numb = "none";
+	}
+	contactInfo::contactInfo(string xname, string xcontact, string xresidence, string xwnum, string xnumb){
+		name = xname;
+		contact = xcontact;
+		residence = xresidence;
+		wnum = xwnum;
+		numb = xnumb;
+	}
+	contactInfo::contactInfo(const contactInfo& copp){
+		name = copp.name;
+		contact = copp.contact;
+		residence = copp.residence;
+		wnum = copp.wnum;
+		numb = copp.numb;
+	}
+	virtual~contactInfo(void){}
+	contactInfo& operator = (const contactInfo& copp){
+		if (this != &copp){
+			name = copp.name;
+			contact = copp.contact;
+			residence = copp.residence;
+			wnum = copp.wnum;
+			numb = copp.numb;
+		}
+		return *this;
+	}
+
+private:
+	string name, contact, residence, wnum, numb;
+
+};
+#endif
